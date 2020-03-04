c24d9d7005bf72c5f41b23b338ebb655e708930f
jmlhermitte <jmlhermittetelecom@gmail.com>
2020-03-04 14:57:12 +0100

    menu update

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var button = document.getElementById('copy')
 
-function copyStringToClipboard (str) {
-   // Create new element
-   var el = document.createElement('textarea')
-   // Set value (string to be copied)
-   el.value = str
-   // Set non-editable to avoid focus and move outside of view
-   el.setAttribute('readonly', '')
-   el.style = {position: 'absolute', left: '-9999px'}
-   document.body.appendChild(el)
-   // Select text inside element
-   el.select()
-   // Copy text to clipboard
-   document.execCommand('copy')
-   // Remove temporary element
-   document.body.removeChild(el)
+function copyStringToClipboard(str) {
+    // Create new element
+    var el = document.createElement('textarea')
+    // Set value (string to be copied)
+    el.value = str
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '')
+    el.style = {
+        position: 'absolute',
+        left: '-9999px'
+    }
+    document.body.appendChild(el)
+    // Select text inside element
+    el.select()
+    // Copy text to clipboard
+    document.execCommand('copy')
+    // Remove temporary element
+    document.body.removeChild(el)
 }
 
 button.addEventListener('click', function () {
@@ -36,3 +39,9 @@ var snippets = {
 </body>`
     }
 }
+
+
+
+$.each(snippets, function (key, value) {
+    $("#languages_list").append('<li><a href="#", onclick="changeSelectedLanguage(\'' +key+ '\')">' +key+ '</a></li>');
+})
