4ae2bc7d05adf9027ac32763b6e60d93f49b2622
Luca Monteilh <lmonteilh@etu.imt-lille-douai.fr>
2020-03-06 10:06:11 +0100

    more snippets

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ var snippets = {
 }`,
         variable: `var test = "successfull";`,
 
+        "Copy string to clipboard": `function copyStringToClipboard(str) {
+    // Create new element
+    var el = document.createElement('textarea')
+    // Set value (string to be copied)
+    el.value = str
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '')
+    el.style = {
+        position: 'absolute',
+        left: '-9999px'
+    }
+    document.body.appendChild(el)
+    // Select text inside element
+    el.select()
+    // Copy text to clipboard
+    document.execCommand('copy')
+    // Remove temporary element
+    document.body.removeChild(el)
+}`,
+
         "Lazy loading image": `!function(window){
   var $q = function(q, res){
         if (document.querySelectorAll) {
@@ -102,8 +122,53 @@ var snippets = {
 
 }(this);`
     },
+
     "Html": {
+
+        "Markup example": `<form id="myForm" action="#" method="post">
+
+  <div>
+    <label for="name">Text Input:</label>
+    <input type="text" name="name" id="name" value="" tabindex="1">
+  </div>
+
+  <div>
+    <h4>Radio Button Choice</h4>
+
+    <label for="radio-choice-1">Choice 1</label>
+    <input type="radio" name="radio-choice" id="radio-choice-1" tabindex="2" value="choice-1">
+
+    <label for="radio-choice-2">Choice 2</label>
+    <input type="radio" name="radio-choice" id="radio-choice-2" tabindex="3" value="choice-2">
+  </div>
+
+  <div>
+    <label for="select-choice">Select Dropdown Choice:</label>
+    <select name="select-choice" id="select-choice">
+      <option value="Choice 1">Choice 1</option>
+      <option value="Choice 2">Choice 2</option>
+      <option value="Choice 3">Choice 3</option>
+    </select>
+  </div>
+
+  <div>
+    <label for="textarea">Textarea:</label>
+    <textarea cols="40" rows="8" name="textarea" id="textarea"></textarea>
+  </div>
+
+  <div>
+    <label for="checkbox">Checkbox:</label>
+    <input type="checkbox" name="checkbox">
+  </div>
+
+  <div>
+    <input type="submit" value="Submit">
+  </div>
+
+</form>`,
+
         head: `<head></head>`,
+
         body: `<body>
     <div></div>
 </body>`
